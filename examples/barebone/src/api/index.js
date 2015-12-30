8ed13adb183abe437b83b8755129544ace08e5e5
David Oliveros <dato.oliveros@gmail.com>
2015-12-30 04:58:35 -0600

    Removed config in barebone example

--- examples/barebone/src/api/index.js
+++ examples/barebone/src/api/index.js
@@ -1,15 +1,10 @@
 var isomorphine = require('isomorphine');
 
-// This will provide the entities in this folder,
-// similar to 'require-all' but in a browser-compatible way
-var morphine = isomorphine.proxy();
-
-// You need to tell isomorphine where your host is. These are the defaults
-morphine.config({
-  host: '127.0.0.1',
-  port: '3000'
-});
-
-// You should require this file directly from the browser,
-// as it will let you use server-side modules located in this folder remotely.
-module.exports = morphine;
+/**
+ * This will provide the entities in this folder.
+ * It's similar to 'require-all' but in a browser-compatible way.
+ *
+ * You should require this file directly from the browser,
+ * as it will let you use server-side modules located in this folder remotely.
+ */
+module.exports = isomorphine.proxy();
